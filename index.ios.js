8c560af503c8fd6100beff91736f1e8c890b4bf9
Glib Kechyn <glkes@yahoo.com>
2017-03-07 15:33:45 +0200

    changes

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,9 @@
  */
 
 import React, { Component } from 'react';
+import { branch, root } from 'baobab-react/higher-order';
+import App from './app';
+import Tree from './tree';
 import {
   AppRegistry,
   StyleSheet,
@@ -12,42 +15,11 @@ import {
   View
 } from 'react-native';
 
-export default class MemberCards extends Component {
+const MemberCards = React.createClass({
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    return (<App />);
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
-AppRegistry.registerComponent('MemberCards', () => MemberCards);
+AppRegistry.registerComponent('MemberCards', () => root(Tree, MemberCards));
+
