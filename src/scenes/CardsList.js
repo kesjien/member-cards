1dac7ed323c3d482c92f6c29c88eceacf4007521
Glib Kechyn <glkes@yahoo.com>
2017-03-16 17:38:48 +0200

    styling

--- src/scenes/CardsList.js
+++ src/scenes/CardsList.js
@@ -15,7 +15,6 @@ import {
 import clamp from 'clamp';
 import Card from '../swipeCards/card';
 import Dimensions from 'Dimensions';
-import styles from './styles';
 
 var SWIPE_THRESHOLD = 100;
 
@@ -264,6 +263,43 @@ class SwipeCards extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FBFAFA',
+  },
+  row: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  noTextView: {
+    flex: 1,
+    backgroundColor:'#ebeef0',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 15,
+    alignItems: 'center',
+  },
+  mt: {
+    marginTop: 20,
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 500,
+  },
+  cardContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+});
+
 
 SwipeCards.propTypes = {
     cards: React.PropTypes.array,
