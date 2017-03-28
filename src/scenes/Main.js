ac2d4d3ac7fb383330346d1a737cde278b6ffa8c
Glib Kechyn <glkes@yahoo.com>
2017-03-28 23:02:16 +0300

    swiper

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -5,12 +5,13 @@ import {
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { branch } from 'baobab-react/higher-order';
+import Swiper from './Swiper';
 
 class Main extends Component {
   render() {
     return (
       <View>
-        <Text>ASDKAJDKAJDLJASDJKASDASJDJLASDKJASDJL</Text>
+        <Swiper />
       </View>
     )
   }
