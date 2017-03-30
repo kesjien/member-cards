b51afc89e08e221882fe07ac612df222e650d526
Glib Kechyn <glkes@yahoo.com>
2017-03-30 17:10:55 +0300

    firebase

---
+++ src/configs/firebase_config.js
@@ -0,0 +1,10 @@
+
+const config = {
+  apiKey: "AIzaSyCq3g3Um50ANIseUsF1Os8qjGVCO6sTy70",
+  authDomain: "androidapp-c0079.firebaseapp.com",
+  databaseURL: "https://androidapp-c0079.firebaseio.com",
+  storageBucket: "androidapp-c0079.appspot.com",
+  messagingSenderId: "625037551529"
+};
+
+export default config;
