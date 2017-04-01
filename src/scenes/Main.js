062d8d6293f08699a6463060f5978cfb1f68e7d9
Glib Kechyn <glkes@yahoo.com>
2017-04-01 17:32:14 +0300

    firebase

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -6,9 +6,16 @@ import {
 import { Actions } from 'react-native-router-flux';
 import { branch } from 'baobab-react/higher-order';
 import Swiper from './Swiper';
+import FirebaseCongif from '../configs/firebase_config.js';
+import Firebase from 'firebase';
 
 class Main extends Component {
+  constructor() {
+    super();
+    Firebase.initializeApp(FirebaseConfig)
+  }
   render() {
+
     return (
       <View>
         <Swiper />
