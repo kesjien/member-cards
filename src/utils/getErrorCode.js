518f95f0381518077f12b80be02b682d4ec71544
Glib Kechyn <glkes@yahoo.com>
2017-04-21 16:50:59 +0300

    errors

--- src/utils/getErrorCode.js
+++ src/utils/getErrorCode.js
@@ -1,12 +1,5 @@
 export default function(error) {
-  const codemsg = {
-    400: "Systemet er ikke tilgængeligt. Prøv igen senere. (400).", // "SERVER_TEMP_UNAVAILABLE",
-    401: 'Ugyldigt CPR-nr. eller pinkode.', //"INVALID_CPR_CODE",
-    402: 'Systemet er ikke tilgængeligt. Prøv igen senere. (402).', //"UNKNOWN_DEVICE",
-    404: 'Ugyldig SMS kode.', //"INVALID_SMS_CODE",
-    405: 'Din adgang er blokeret. Kontakt venligst SEB Pension. (405).', // "USER_LOCKED",
-    500: 'Systemet er ikke tilgængeligt. Prøv igen senere. (500).', //"SERVER_ERROR",
-  };
+  const codemsg = {};
 
   return {
     msg: codemsg[+error.message] || `ERROR: ${error.message}`
